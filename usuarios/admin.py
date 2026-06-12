10a11bcc807967bb25142c13773bc3fb484421a6
Enderson <endersonjackson2@gmail.com>
2026-06-12 15:18:17 -0300

    ADD: Admin Usuarios

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.urls import reverse
+from django.utils.html import format_html
 
 from .forms import UserProfileAdminForm
 from .models import UserProfile
@@ -21,6 +24,33 @@ class UserProfileInline(admin.StackedInline):
 
 class UserAdmin(BaseUserAdmin):
     inlines = (UserProfileInline,)
+    list_display = (
+        'username',
+        'membro_vinculado',
+        'email',
+        'first_name',
+        'last_name',
+        'is_staff',
+    )
+    search_fields = BaseUserAdmin.search_fields + (
+        'perfil__membro__nome_completo',
+        'perfil__membro__nome_conhecido',
+        'perfil__membro__cpf',
+    )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('perfil__membro')
+
+    @admin.display(description='Membro vinculado', ordering='perfil__membro__nome_completo')
+    def membro_vinculado(self, obj: User):
+        try:
+            perfil = obj.perfil
+        except ObjectDoesNotExist:
+            return '—'
+        if not perfil.membro_id:
+            return '—'
+        url = reverse('admin:membros_membro_change', args=[perfil.membro_id])
+        return format_html('<a href="{}">{}</a>', url, perfil.membro)
 
 
 if admin.site.is_registered(User):
